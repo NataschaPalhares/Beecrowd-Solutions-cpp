ec1e84162ec3762d48b1d978cb706679fe5b3ba9
NataschaPalhares <88913342+NataschaPalhares@users.noreply.github.com>
2021-12-29 20:34:22 -0300

    Create B.cpp

---
+++ Contest/Contest de Natal 2021/B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	if (n <= 25)
+		cout << "Recebe Presente, Feliz Natal!\n";
+	else
+		cout << "Recebe Carvao\n";
+
+	return 0;
+}
